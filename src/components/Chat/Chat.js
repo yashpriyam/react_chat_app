34c75462002a672e2a7d98b0dc738ccda5a9ee98
yashpriyam <yashpriyam74@gmail.com>
2020-05-06 13:35:31 +0530

    changing backend endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'https://mychatthing.herokuapp.com/';
+  const ENDPOINT = 'https://reactchatapp-backend.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
